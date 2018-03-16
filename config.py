dc874de1dbb8985bd4477623bc16cf74391da09b
J.Kuai <congjinruo@outlook.com>
2018-03-16 20:05:35 +0800

    delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-class Config:
-    REDIS_KEY = "your auth"
-    REDIS_SERVER = "localhost"
-
-    MARIADB_SERVER = "mysql+mysqlconnector://your user@your host/your database"

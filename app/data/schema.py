d15c8d3a838c105389f716637a0429818185e8eb
J.Kuai <congjinruo@outlook.com>
2018-03-19 00:53:41 +0800

    ver 1.5.3 fix search

--- app/data/schema.py
+++ app/data/schema.py
@@ -49,7 +49,7 @@ class Query(graphene.ObjectType):
             #query = query.filter(or_(BookModel.book_name.like('%%%s%%' % args.get('search')), BookModel.author.like('%%%s%%' % args.get('search')))).all()
             bookList_a = query.filter(BookModel.book_name.like('%%%s%%' % args.get('search'))).all()
             bookList_b =query.filter(BookModel.author.like('%%%s%%' % args.get('search'))).all()
-            bookList_c = [].extend(bookList_a)
+            bookList_c = [] + bookList_a
             for x in bookList_a:
                 for y in bookList_b:
                     if x.book_id != y.book_id:

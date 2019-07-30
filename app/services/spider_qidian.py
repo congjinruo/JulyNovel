feff0adfecab1c21728fc177c94621b9b8707bbd
Kuai <congjinruo@outlook.com>
2019-07-30 13:28:49 +0800

    后端新增github登录接口

--- app/services/spider_qidian.py
+++ app/services/spider_qidian.py
@@ -108,7 +108,7 @@ class QidianSpider:
         book['summary'] = summary_tag.text.strip()
         
         #for volumn_tag in volumn_tags:
-            #print(volumn_tag.contents[2].strip())
+        #print(volumn_tag.contents[2].strip())
         request_url_list = []
         chapters = []
         count = len(chapter_tags)
@@ -120,7 +120,7 @@ class QidianSpider:
             for cpt in chapters:
                 request_url = "https://vipreader.qidian.com/chapter/%s/%s" % (book['xbookId'], cpt["xchapterId"])
 
-                content_key = "%s|content|%s|%s" % (self.siteId, book['xbookId'], cpt['xchapterId'])
+                #content_key = "%s|content|%s|%s" % (self.siteId, book['xbookId'], cpt['xchapterId'])
                 #内容章节如果已经存在，那么不再去爬取，减小服务器压力
                 #if self.mRedis.isValidKey(content_key):
                     #continue
@@ -149,7 +149,7 @@ class QidianSpider:
                     book["lastupdate"] = chapter['updatetime']
 
                 chapters.append(chapter)
-                content_key = "%s|content|%s|%s" % (self.siteId, book['xbookId'], chapter['xchapterId'])
+                #content_key = "%s|content|%s|%s" % (self.siteId, book['xbookId'], chapter['xchapterId'])
                 #if self.mRedis.isValidKey(content_key):
                     #continue
                 request_url_list.append(request_url)
--- app/views.py
+++ app/views.py
@@ -3,7 +3,10 @@ Routes and views for the flask application.
 """
 # -*- coding: utf-8 -*-
 import os
-from flask import Flask, render_template, redirect, url_for, send_from_directory
+import requests
+from config import Config
+from urllib import parse
+from flask import Flask, render_template, redirect, url_for, send_from_directory,request
 from flask_graphql import GraphQLView
 from .data.base import db_session
 from .data.schema import schema
@@ -30,6 +33,31 @@ def index():
 
     return app.send_static_file('index.html')
 
+@app.route('/login/<key>', methods=["GET", "POST"])
+def login(key):
+    """
+    OSS 删除匹配key的所有文件
+    """
+    if key is None:
+        return 'fail'
+
+    print(key)
+
+    if key == 'github':
+        print(request.args.get('code'))
+        parar={'client_id': Config.GITHUB_CLIENT_ID, 'client_secret': Config.GITHUB_CLIENT_SECRET, 'code': request.args.get('code')}
+        headers={
+            'Accept':'application/json'
+        }
+        r = requests.post('https://github.com/login/oauth/access_token',params = parar, headers=headers)
+        print(r.text)
+        access_token = r.json()
+        print(access_token)
+        userInfo = requests.get('https://api.github.com/user?access_token=' + access_token['access_token'])
+        return userInfo.text
+
+    return 'success'
+
 @app.route('/missionStart/<key>')
 def missionStart(key):
     if key is None:
@@ -54,6 +82,40 @@ def uploadAll():
     '''
     OSS().upload_all_image()
     return 'success'
+@app.route('/getIPList')
+def getIPList():
+    '''
+    获取ip所属地区
+    '''
+    path = 'D:/DingTalk/ipList.txt'
+    res = ''
+    with open(path,'r') as f:
+        content = f.read()
+        ipList = content.split(';')
+        for ip in ipList:
+            if ip is '':
+                continue
+            else:
+                res += getIPLocation(ip) + ';'
+        return res
+
+    return 'fail'
+
+def getIPLocation(ip):
+    parar={'query': ip, 'resource_id': '6006', 'ie': 'utf8', 'format':'json'}
+    headers={
+        'Accept':'application/json'
+    }
+    r = requests.get('https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php',params = parar, headers=headers)
+    response_str = r.json()
+    data = response_str['data']
+    location = '出错了：'
+    if response_str['status'] == '0':
+        item = data[0]
+        location = item['location']
+    else:
+        location += ip
+    return location
 
 @app.route('/delete/<key>')
 def delete(key):
